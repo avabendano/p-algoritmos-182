0aea2807adeb0167541126c0151709a82a9d2ec2
unknown <alisonvicab75@gmail.com>
2018-11-16 12:19:40 -0500

    Ejercicio

--- Polimorfismo/Paquete2/main.py
+++
@@ -1,7 +0,0 @@
-
-from paquete.model import *
-
-lista_jugadores = [Futbolista('Antonio'), MedicoEquipo("Ramon"), PresidenteEquipo("Francisco"), Entrenador("Jóse")]
-
-for l in lista_jugadores:
-    l.entrenamiento()
